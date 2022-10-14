b04495e868ac6c41ee52f70d88b13e2dbda2ef80
Julio Pampuch <juliopampuch@gmail.com>
2022-10-13 21:16:39 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/JulioPampuch/design-system'
+    }
+    return config
   }
 }
