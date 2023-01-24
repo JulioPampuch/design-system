0e7721f908196a820a84142ef72348996b0b84d0
Julio Pampuch <juliopampuch@gmail.com>
2023-01-23 23:01:31 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
-import { Heading } from './components/heading'
-import { Text } from './components/text'
-import { Logo } from './components/logo'
 import './styles/global.css'
-import { Envelope, Lock, TextIndent } from 'phosphor-react'
-import { TextInput } from './components/textInput'
-import { Checkbox } from './components/checkbox'
-import { Button } from './components/button'
 import { SigIn } from './pages/sigIn'
 
 export function App() {
